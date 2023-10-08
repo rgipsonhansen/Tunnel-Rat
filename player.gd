5a9feaf4376a813d0387488927a668565f0227c0
skrakowski <bastiankraken@gmail.com>
2023-10-07 21:33:47 -0600

    Added 'tank controls' ie rotation of camera

--- player.gd
+++ player.gd
@@ -3,7 +3,7 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
-
+@export var turn_speed = 0.025
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
@@ -21,11 +21,17 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "forward", "back")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+		
+	if Input.is_action_pressed('rotateLeft'):
+		rotation.y +=  .05
+	elif Input.is_action_pressed('rotateRight'):
+		rotation.y -= .05
 
 	move_and_slide()
