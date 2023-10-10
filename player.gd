315b4dc6916e5ade941c7cbbb2cf7ce573a625a7
skrakowski <bastiankraken@gmail.com>
2023-10-10 12:39:28 -0600

    Added y axis mouse camera movement

--- player.gd
+++ player.gd
@@ -1,12 +1,24 @@
 extends CharacterBody3D
 
+@onready var head = $Head
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const MOUSE_SENS = 0.25
 @export var turn_speed = 0.025
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)	
+
+#mouse movement on the y axis
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x * MOUSE_SENS))
+		#head.rotate_x(deg_to_rad(-event.relative.y * MOUSE_SENS))
+		#head.rotation_x = clamp(head.rotation.x,deg_to_rad(-89),deg_to_rad(89))
+	
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -21,17 +33,25 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "forward", "back")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-
+	
+	
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-		
+	
+#Tank control movement
 	if Input.is_action_pressed('rotateLeft'):
 		rotation.y +=  .05
 	elif Input.is_action_pressed('rotateRight'):
 		rotation.y -= .05
+	#elif InputEventMouseMotion:
+		#rotate_object_local(Vector3.UP, event.relative.x * .001)
+		#rotate_object_local(Vector3.RIGHT, event.relative.y * 0.001)
+		
+
+
 
 	move_and_slide()
